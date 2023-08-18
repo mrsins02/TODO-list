9872e6f961707188148193d57b6daf6aec3f6dda
mrsins02 <amin.d.d.y1357@gmail.com>
2023-08-18 03:40:52 +0330

    post method added to home view; create method of todo serializer overwrited; serializers modified

--- todo/views.py
+++ todo/views.py
@@ -9,6 +9,16 @@ from .serializers import TodoSerializer, TodoDetailSerializer
 class TodoListAPIView(APIView):
     def get(self, request):
         todo = Todo.objects.all()
-        todo_ser = TodoSerializer(instance=todo, many=True)
+        todo_serializer = TodoSerializer(instance=todo, many=True)
 
-        return Response(data=todo_ser.data, status=status.HTTP_200_OK)
+        return Response(data=todo_serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        todo_serializer = TodoSerializer(data=request.data)
+        if todo_serializer.is_valid():
+            todo_serializer.validated_data["user"] = request.user
+            result_data = todo_serializer.create(todo_serializer.validated_data)
+            result_serializer = TodoSerializer(instance=result_data)
+            return Response(data=result_serializer.data, status=status.HTTP_200_OK)
+
+        return Response(data=todo_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
